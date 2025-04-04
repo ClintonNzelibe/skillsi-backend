9c98d7e49871b677d41feda4855c7dc0f10bb0d2
BlinksCrib <ajibolaisaac09@gmail.com>
2025-04-04 19:49:33 +0100

    fix: vercel deployment

--- index.ts
+++ index.ts
@@ -135,7 +135,7 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5500;
-// if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
 
   // Connect to database first, then start server
   connectToDatabase(connectionString)
@@ -151,6 +151,6 @@ const port = process.env.PORT || 5500;
   // app.listen(port, () => {
   //   console.log(`Server is listening on port ${port}...`);
   // });
-// }
+}
 
 export default app;
