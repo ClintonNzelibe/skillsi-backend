6039061005b0816dc354a2b77782f423641973f4
BlinksCrib <ajibolaisaac09@gmail.com>
2025-05-23 12:31:02 +0100

    feat: increased rate limiter

--- routes/userAuthRoutes.ts
+++ routes/userAuthRoutes.ts
@@ -7,7 +7,7 @@ import rateLimiter from "express-rate-limit";
 
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000,
-  limit: 5,
+  limit: 50000,
   standardHeaders: "draft-7",
   legacyHeaders: false,
   message: "Too many requests from this IP, please try again after 15 minutes",
