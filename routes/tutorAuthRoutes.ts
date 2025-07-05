59119ef2ac309baabdcd4ed31c777f23255c6424
BlinksCrib <ajibolaisaac09@gmail.com>
2025-07-05 20:13:28 +0100

    fix: course changes

--- routes/tutorAuthRoutes.ts
+++ routes/tutorAuthRoutes.ts
@@ -7,16 +7,25 @@ import rateLimiter from "express-rate-limit";
 
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000,
-  limit: 5,
+  limit: 3000,
   standardHeaders: "draft-7",
   legacyHeaders: false,
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
-import { signupTutor, signinTutor, } from "../controllers/tutorAuthController.js";
+import {
+  signupTutor,
+  verifyEmail,
+  resendToken,
+  signinTutor,
+  finishOnboarding,
+} from "../controllers/tutorAuthController.js";
 
 router.route("/signupTutor").post(signupTutor);
+router.route("/verifyEmail").post(verifyEmail);
+router.route("/resendToken").post(resendToken);
 router.route("/signinTutor").post(apiLimiter, signinTutor);
+router.route("/finishOnboarding").post(finishOnboarding);
 // router.route("/logout").post(authenticateUser, logout);
 
 export default router;
