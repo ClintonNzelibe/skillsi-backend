365310aeb9fa4053250b5529bc870a19f3c505ad
BlinksCrib <ajibolaisaac09@gmail.com>
2025-08-25 12:19:19 +0100

    fix: cors for patch

--- index.ts
+++ index.ts
@@ -119,7 +119,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-API-Key"],
     preflightContinue: false,
     optionsSuccessStatus: 204,
